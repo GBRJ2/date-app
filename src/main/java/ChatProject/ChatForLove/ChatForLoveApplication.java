7431fc3c97325959c110ee6e2d8937cbb60ff391
GyeongMo_Kim <88998289+k-mozzi@users.noreply.github.com>
2024-09-19 14:49:45 +0900

    test

---
+++ src/main/java/ChatProject/ChatForLove/ChatForLoveApplication.java
@@ -0,0 +1,13 @@
+package ChatProject.ChatForLove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatForLoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatForLoveApplication.class, args);
+	}
+
+}
